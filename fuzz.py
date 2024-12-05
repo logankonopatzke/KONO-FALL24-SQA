2db21c292256889dc6abff8159add5854317fa91
LOGAN KONOPATZKE <logankono9080@gmail.com>
2024-12-05 15:00:35 -0600

    test without linter and update lessons learned

--- fuzz.py
+++ fuzz.py
@@ -4,6 +4,7 @@ import ast
 from datetime import datetime
 import time
 import sys
+sys.path.insert(1, '/home/konopatzke/Desktop/KONO-FALL24-SQA/MLForensics-farzana/FAME-ML')
 import lint_engine
 import py_parser
 import main
